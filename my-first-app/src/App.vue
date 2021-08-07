1a076f18c96cd79c4ccb3abf123df7a192dd84cd
Yevgeniy <zhe.tovtin@gmail.com>
2021-08-07 09:00:38 -0500

    created component ApartmentsItem

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div :id="$style.app"></div>
+  <div :id="$style.app">
+    <ApartmentsItem
+      :descr="apartment.descr"
+      :price="apartment.price"
+      :rating="apartment.rating"
+      imgSrc="https://cdn.pixabay.com/photo/2021/07/16/18/28/czech-republic-6471576_960_720.jpg"
+    />
+  </div>
 </template>
 
 <script>
+import ApartmentsItem from "./components/apartment/ApartmentsItem";
 export default {
-  name: 'App',
-  components: {},
+  name: "App",
+  components: {
+    ApartmentsItem,
+  },
+  data() {
+    return {
+      apartment: {
+        id: "1232123",
+        title: " lkf slfk sdlk jslfk j",
+        descr: "lkf lskf jlsakfj ñlask fjñslk fsalñkf jslñdk fñlsk fj",
+        price: 2032,
+        rating: 4.5,
+        location: {
+          city: "Tokyo",
+        },
+        owner: {
+          name: "Elen",
+          phone: "2323 2323 ",
+          email: "email@gmail.com",
+        },
+      },
+    };
+  },
 };
 </script>
 
