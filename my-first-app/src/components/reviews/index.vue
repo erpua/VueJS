8cdd3d08dd3848b50736973962f29bce7279e732
Yevgeniy <zhe.tovtin@gmail.com>
2021-09-16 10:36:41 -0500

    TODO:// fix bug with props

--- my-first-app/src/components/reviews/index.vue
+++ my-first-app/src/components/reviews/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <section class="reviews">
+    <ReviewsItem
+      v-for="review in reviews"
+      :key="review.author"
+      :review="review"
+    />
+  </section>
 </template>
 
 <script>
+import ReviewsItem from './reviews-item';
 export default {
-  name: '',
+  name: 'Reviews',
+  components: {
+    ReviewsItem,
+  },
+  props: {
+    review: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
--- my-first-app/src/pages/Apartment.vue
+++ my-first-app/src/pages/Apartment.vue
@@ -3,10 +3,13 @@
     <Container>
       <div class="apartment-page__content">
         <ApartmentsMainInfo :apartment="apartment" />
-        <AparmentsOwner
-          class="apartment-page__owner"
-          :owner="apartment.owner"
-        />
+        <div class="apartment-page__additional-info">
+          <AparmentsOwner
+            class="apartment-page__owner"
+            :owner="apartment.owner"
+          />
+          <Reviews :reviews="reviewsList" />
+        </div>
       </div>
     </Container>
   </main>
@@ -17,6 +20,8 @@ import Container from '../components/shared/Container';
 import apartments from '../components/apartment/apartments';
 import ApartmentsMainInfo from '../components/apartment/ApartmentsMainInfo';
 import AparmentsOwner from '../components/apartment/ApartmentsOwner';
+import Reviews from '../components/reviews';
+import reviewsList from '../components/reviews/reviews.json';
 
 export default {
   name: 'ApartmentPage',
@@ -24,9 +29,13 @@ export default {
     Container,
     ApartmentsMainInfo,
     AparmentsOwner,
+    Reviews,
   },
   /* to choose spesific apartment through id use computed: */
   computed: {
+    reviewsList() {
+      return reviewsList;
+    },
     apartment() {
       return apartments.find(
         (apartment) => apartment.id === this.$route.params.id
