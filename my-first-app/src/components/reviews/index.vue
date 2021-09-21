c18d755da0b3920e37845a4079f6b987fd27452b
Yevgeniy <zhe.tovtin@gmail.com>
2021-09-20 22:04:24 -0500

    updated component Reviews

--- my-first-app/src/components/reviews/index.vue
+++ my-first-app/src/components/reviews/index.vue
@@ -3,16 +3,18 @@
     <div class="reviews__heading">
       <h2 class="reviews__title">Summary rating</h2>
       <div class="reviews__rating">
-        <span>98 feedbacks</span>
-        <Rating :rating="0" />
+        <span>{{ amountOfreviews }} feedback(s)</span>
+        <Rating :rating="totalRating" />
       </div>
     </div>
     <ReviewsItem
-      v-for="review in reviews"
+      v-for="review in currentReviews"
       :key="review.author"
       :review="review"
     />
-    <button class="reviews__show-more">Read more...</button>
+    <button @click="toggleReviews" class="reviews__show-more">
+      {{ buttonText }}
+    </button>
   </section>
 </template>
 
@@ -32,9 +34,43 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      reviewsLimit: 2,
+    };
+  },
   computed: {
     totalRating() {
-      return this.reviews.reduce((acc, review) => acc + review.rating);
+      const total = this.reviews.reduce(
+        (acc, review) => acc + review.rating,
+        0
+      );
+      return total / this.reviews.length;
+    },
+    amountOfreviews() {
+      return this.reviews.length;
+    },
+    currentReviews() {
+      return this.reviews.slice(0, this.reviewsLimit);
+    },
+    buttonText() {
+      return this.reviewsLimit === this.reviews.length
+        ? 'Minimize'
+        : 'Read more ...';
+    },
+  },
+  /* methods: {
+    toggleReviews() {
+      this.reviewsLimit = this.reviews.length;
+    },
+  }, */
+  methods: {
+    toggleReviews() {
+      if (this.reviewsLimit === this.reviews.length) {
+        this.reviewsLimit = 2;
+        return;
+      }
+      this.reviewsLimit = this.reviews.length;
     },
   },
 };
