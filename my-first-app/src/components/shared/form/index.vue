bd67a86f9449c851fb6335730df1a99d58b281ac
Yevgeniy <zhe.tovtin@gmail.com>
2021-10-25 22:02:14 -0500

    started validation of input

---
+++ my-first-app/src/components/shared/form/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <form class="form">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  //provide => for having it is own
+  provide: {
+    form: this,
+    methods: {
+      registerInput() {},
+      unRegisterInput() {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
